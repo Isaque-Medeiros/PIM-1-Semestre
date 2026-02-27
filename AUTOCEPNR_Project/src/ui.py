0324457f67794dc762c4c61cb69241d055e31436
Isaque de Medeiros Pinto <132860343+Isaque-Medeiros@users.noreply.github.com>
2026-02-27 20:23:43 +0000

    FirstlProjectSDDMetodologia

--- AUTOCEPNR_Project/src/ui.py
+++
@@ -1,7 +0,0 @@
-"""User interface components (GTK/Tk/Win32)."""
-
-
-def show_status(message: str):
-    """Display a transient message to the user."""
-    # TODO: implement actual window or tray icon
-    print(message)
